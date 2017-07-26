aadf1fd7ac53ed1531ff70105739d196f5ffce76
Juho <osku_321@hotmail.com>
2017-07-27 00:56:52 +0300

    Reorganized project paths

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jassoo
 Want to add random quotes on random images? 💁 Me too!
+
+https://github.com/umbrae/reddit-top-2.5-million
--- jassoo.py
+++ jassoo.py
@@ -1,13 +1,18 @@
 #!/usr/bin/python3
 
+from PIL import Image
+from PIL import ImageFont
+from PIL import ImageDraw
 import urllib.request
 import json
 
 API_URL = "https://api.unsplash.com/photos/random/?client_id="
 APPLICATION_ID = "c08cd20813787def6e8c4fb5da974394afea0763c16cb04db67766fa5e828904"
+IMAGES_PATH = "resources/images/"
+
 
 def load_image(url):
-    urllib.request.urlretrieve(url, "images/img0.jpg")
+    urllib.request.urlretrieve(url, IMAGES_PATH+"img0.jpg")
 
 def random_image_url():
     response = urllib.request.urlopen(API_URL+APPLICATION_ID)
@@ -15,7 +20,15 @@ def random_image_url():
     json_result = json.loads(data.decode('utf-8'))
     return json_result['urls']['raw']
 
+def draw_text():
+    image = Image.open(IMAGES_PATH+"img0.jpg")
+    draw = ImageDraw.Draw(image)
+#    font = ImageFont.truetype("serif", 16)
+    draw.text((0, 0), "Sample texti", (255, 255, 255))
+    image.save(IMAGES_PATH+'img1.jpg')
+
 def main():
     load_image(random_image_url())
+    draw_text()
 
 main()
